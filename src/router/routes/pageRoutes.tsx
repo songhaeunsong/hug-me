07383e868c9a884668f1c8c43ee520038677c87d
songhaeunsong <she3469@gmail.com>
2025-08-17 20:49:12 +0900

    chore: 위험 분석 path명 변경

--- src/router/routes/pageRoutes.tsx
+++ src/router/routes/pageRoutes.tsx
@@ -1,11 +1,11 @@
 import { CheckList } from '@/pages/CheckList';
 import { DepositCheck } from '@/pages/DepositCheck';
 import { Quiz } from '@/pages/Quiz';
-import { RiskAnalysis } from '@/pages/RiskAnalysis';
+import { RiskPrediction } from '@/pages/RiskPrediction';
 
 export const pageRoutes = [
   { path: 'check-list', element: <CheckList />, handle: { title: '체크리스트' } },
   { path: 'quiz', element: <Quiz />, handle: { title: '퀴즈' } },
-  { path: 'risk-analysis', element: <RiskAnalysis />, handle: { title: '위험 분석' } },
+  { path: 'risk-prediction', element: <RiskPrediction />, handle: { title: '위험 분석' } },
   { path: 'deposit-check', element: <DepositCheck />, handle: { title: '보증금 점검' } },
 ];
