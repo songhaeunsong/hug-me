fdfa292a08f9103540b0a1ee99d81ceade30c09f
songhaeunsong <she3469@gmail.com>
2025-08-17 16:32:12 +0900

    chore: shadcn component 추가

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,27 @@
+import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
+import { CheckIcon } from 'lucide-react';
+import * as React from 'react';
+
+import { cn } from '@/lib/utils';
+
+function Checkbox({ className, ...props }: React.ComponentProps<typeof CheckboxPrimitive.Root>) {
+  return (
+    <CheckboxPrimitive.Root
+      data-slot="checkbox"
+      className={cn(
+        'peer border-[#999999] dark:bg-input/30 data-[state=checked]:bg-point data-[state=checked]:border-point data-[state=checked]:text-primary-foreground dark:data-[state=checked]:bg-point size-5 shrink-0 rounded-[4px] border-[1.5px] shadow-xs transition-shadow outline-none disabled:cursor-not-allowed disabled:opacity-50',
+        className,
+      )}
+      {...props}
+    >
+      <CheckboxPrimitive.Indicator
+        data-slot="checkbox-indicator"
+        className="flex items-center justify-center text-current transition-none"
+      >
+        <CheckIcon className="size-4" />
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  );
+}
+
+export { Checkbox };
