3c9a65b87524366e70e49e9d19b132c81e927f54
songhaeunsong <she3469@gmail.com>
2025-08-17 16:33:44 +0900

    feat: 뒤로가기 버튼 구현

---
+++ src/components/common/BackButton.tsx
@@ -0,0 +1,7 @@
+import { useNavigate } from 'react-router-dom';
+
+export const BackButton = () => {
+  const navigate = useNavigate();
+
+  return <button onClick={() => navigate(-1)}>{'<-'}</button>;
+};
