a9864a5dc07fa557275bf9769b7709c8a282495d
songhaeunsong <she3469@gmail.com>
2025-08-19 10:40:55 +0900

    feat: svg 아이콘 적용(홈 네비게이션 및 백 버튼)

--- src/components/common/BackButton.tsx
+++ src/components/common/BackButton.tsx
@@ -3,5 +3,9 @@ import { useNavigate } from 'react-router-dom';
 export const BackButton = () => {
   const navigate = useNavigate();
 
-  return <button onClick={() => navigate(-1)}>{'<-'}</button>;
+  return (
+    <button className="cursor-pointer" onClick={() => navigate(-1)}>
+      <object className="pointer-events-none" data="icons/arrow_left.svg" />
+    </button>
+  );
 };
--- src/components/common/NavigationBar.tsx
+++ src/components/common/NavigationBar.tsx
@@ -4,19 +4,27 @@ export const NavigationBar = () => {
   return (
     <ul className="w-full grid grid-cols-4 sm:text-[13px] text-[10px] border-b-[1px] p-4 wrap-words">
       <Link to="/check-list" className="flex flex-col justify-start items-center gap-2 py-4 mx-2">
-        <div className="w-[45px] h-[45px] bg-gray-500/20"></div>
+        <div className="h-[45px]">
+          <object className="pointer-events-none" data="icons/list_alt_check.svg" />
+        </div>
         <span>계약 체크리스트</span>
       </Link>
       <Link to="/quiz" className="flex flex-col justify-start items-center gap-2 py-4 mx-2">
-        <div className="w-[45px] h-[45px] bg-gray-500/20"></div>
+        <div className="h-[45px]">
+          <object className="pointer-events-none" data="icons/quiz.svg" />
+        </div>
         <span>퀴즈</span>
       </Link>
-      <Link to="/risk-analysis" className="flex flex-col justify-start items-center gap-2 py-4 mx-2">
-        <div className="w-[45px] h-[45px] bg-gray-500/20"></div>
+      <Link to="/risk-prediction" className="flex flex-col justify-start items-center gap-2 py-4 mx-2">
+        <div className="h-[45px]">
+          <object className="pointer-events-none" data="icons/bar_chart.svg" />
+        </div>
         <span>위험도 분석</span>
       </Link>
       <Link to="/deposit-check" className="flex flex-col justify-start items-center gap-2 py-4 mx-2">
-        <div className="w-[45px] h-[45px] bg-gray-500/20"></div>
+        <div className="h-[45px]">
+          <object className="pointer-events-none" data="icons/admin_panel_settings.svg" />
+        </div>
         <span>맞춤 보증 추천</span>
       </Link>
     </ul>
